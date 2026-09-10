95bc839f6c85041d960491e142018b94a2d28e0e
Mark Haiduc <marktudorh@gmail.com>
2026-09-10 14:41:43 -0400

    restructured and updated

--- pycode/gnc/controls.py
+++ pycode/gnc/controls.py
@@ -1,7 +1,6 @@
 #quick note : this is in a class to make sure it is easily accessible to other functions
-#and keep in mind in Py classes do not have the privacy of C++ classes.
+#and keep in mind in Py classes do not have the privacy of C++ classes. So we use the __ to create it ourselves. 
 from time import monotonic, time 
-
 class Control : 
     # use double underscores for maximum security. 
     def __init__(self, kp, ki, kd, intregral, ) : 
@@ -10,7 +9,7 @@ class Control :
         self.__kd = kd
         self.__integral = 0 
         self.__prev_error = 0 
-        self.__prev_time=0 
+        self.__prev_time= 0 
         
     #read only for when needed to access the values of kp, ki, kd, integral, and prev_error.
     @property
@@ -29,17 +28,21 @@ class Control :
     def prev_error(self) : 
         return self.__prev_error 
     
-    #notes : to access kp, ki, or kd, you still use kp ki and kd outside the class. IN THE CLASS use __kp, aka kp WITH the doubld underscore. 
+    #notes : to access kp, ki, or kd, you still use kp ki and kd outside the class. IN THE CLASS use __kp, aka kp WITH the doubld underscore, else it will run into an error.
     
     def PID(self, error) : 
+        #kp for proportional
+        #ki for integral
+        #kd for derivative
         current_t = time.monotonic() 
         dt = current_t - self.__prev_time 
         self.__prev_time = current_t
-        proportional=self.__kp * error #=INSTANT error
+        proportional = self.__kp * error #=INSTANT error
         self.__integral += error * dt # = accumulated error
         derivative = (error - self.__prev_error) / dt if dt> 0 else 0 #no errors on the first frame as we filter dt ; deriv = rate of change 
         output = proportional + (self.__ki * self.__integral) + (self.__kd * derivative)
         return output 
+    
     #this goes to mavlink 
     
     
